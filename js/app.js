2ce5fc5a0f3a9f26ae2a74134353b73275790476
demons1502 <nnhau.20it2@vku.udn.vn>
2021-08-11 14:29:48 +0700

    finshed project

---
+++ js/app.js
@@ -0,0 +1,53 @@
+// Set date
+const date = document.getElementById("date").innerHTML = new Date().getFullYear();
+// Nav toggle
+const navBtn = document.getElementById("nav-toggle");
+const links= document.getElementById("nav-links");
+
+// event listener
+navBtn.addEventListener("click", () => {
+    links.classList.toggle("show-links")
+});
+// Nav fixed
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll",() => {
+    if(window.pageYOffset > 62){
+        navbar.classList.add('fixed');
+    }
+    else {
+        navbar.classList.remove('fixed');
+    }
+})
+// Smooth Scroll
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((link) => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        links.classList.remove("show-links");
+        const id = e.target.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        // position
+        let position;
+        if(navbar.classList.contains("fixed")) {
+            position = element.offsetTop - 62;
+        }
+        else {
+            position = element.offsetTop - 124;
+        }
+        if(window.innerWidth < 992){
+            if(navbar.classList.contains("fixed")) {
+                position = element.offsetTop - 62;
+            }else {
+                position = element.offsetTop - 332 - 62;
+            }
+        }
+        // window scroll to
+        window.scrollTo({
+            left:0,
+            top:position,
+            behavior: "smooth"
+        })
+    })
+})
